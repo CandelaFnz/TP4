514839c5c855380d81abc9503a48200fd5aa00eb
Candela <candelaf98@gmail.com>
2020-09-05 20:12:02 -0300

    Programa terminado

--- tp4.py
+++ tp4.py
@@ -2,6 +2,8 @@
 S = 0
 OS = 0
 AP = 0
+T=0
+TOTAL=0
 EMPLEADOS = []
 X = int(input("Ingrese\n 1.COMENZAR\n 2.CERRAR"))
 while X == 1:
@@ -11,5 +13,17 @@ while X == 1:
     AP = S*11/100
     SUELDO = (S-OS-AP)
     E = int(input("Ingrese:\n 1.SOLTERO\n 2.CASADO CON HIJOS\n 3.CASADO SIN HIJOS"))
-
-    
+    if E == 1:
+        print ("Total a depositar a", N, "es de: $ ", SUELDO)
+        TOTAL=SUELDO
+    if E == 2:
+        TOTAL = SUELDO + 900
+        print ("El total a depositar a", N, "es de: $ ", TOTAL)
+    if E == 3:
+        TOTAL = SUELDO + 500
+        print ("El total a depositar a", N, "es de: $ ", TOTAL)
+    T=T+TOTAL
+    EMPLEADOS.append(N)
+    X = int(input("Ingrese\n 1.CONTINUAR\n 2.CERRAR"))
+print("Empleados: ", EMPLEADOS)
+print("Egresos por sueldos:$ ", T)
